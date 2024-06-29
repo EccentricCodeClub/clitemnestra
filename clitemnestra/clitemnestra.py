39dba30f48d831806f89fee6a89ddeb2d6059cf7
Gabriel Laureano <gabriel.oliveira@simeon.com.br>
2024-06-29 12:34:11 -0300

    v0.0.5

--- clitemnestra/clitemnestra.py
+++ clitemnestra/clitemnestra.py
@@ -6,6 +6,9 @@ import tomlkit
 from rich.console import Console
 from rich.table import Table
 from rich.columns import Columns
+import random
+import os
+from rich.text import Text
 
 # CONSTANTS
 CONFIG_FILE_PATH = "clitemnestra/clitemnestra.toml"
@@ -23,18 +26,62 @@ def main():
 	elif parser.command == 'search':	# Done
 		command_search(parser.term)
 	elif parser.command == 'edit':
-		print("Em Construção")
-	elif parser.command == 'config':
-		print("Em Construção")
+		print("Under Construction")
+	elif parser.command == 'config':	# WIP
+		print("Under Construction")
 	elif parser.command == 'exec':
-		print("Em Construção")
+		print("Under Construction")
 	elif parser.command == 'script':
-		print("Em Construção")
+		print("Under Construction")
 	elif parser.command == 'executor':
-		print("Em Construção")
+		print("Under Construction")
 	else:
-		print("Invalid command")
-		sys.exit(1)
+		# print("Invalid command")
+		# sys.exit(1)
+		default_info()
+
+def default_info():
+	data = [
+		"Clitemnestra",
+		"Version: -",
+		"",
+		"Script management tool for helping you to organize and execute your code.",
+		"Usage: clitemnestra <command> [<args>]",
+		"",
+		"Commands:",
+		"  list <tag>      List scripts",
+		"  search <term>   Search records by term",
+		"  edit            Edit configuration file",
+		"  config          Manage configuration",
+		"  exec <nickname> Execute a script",
+		"  script          Manage scripts",
+		"  executor        Manage executors",
+		"  help            Show this message",
+	]
+
+	# Get the list of files in the assets folder
+	assets_folder = "clitemnestra/assets"
+	files = os.listdir(assets_folder)
+
+	# Pick a random file from the list
+	random_file = random.choice(files)
+
+	# Open the random file and read its contents
+	with open(os.path.join(assets_folder, random_file), "r") as f:
+		logo = f.read()
+
+	display = Table(
+		show_header=False,
+		box=None,
+		safe_box=True
+	)
+
+	console = Console()
+
+	display.add_column("Logo", style="bright_white", vertical='middle')
+	display.add_column("Info", style="white", vertical='middle')
+	display.add_row(Text.from_ansi(logo), '\n'.join(data))
+	console.print(display)
 
 
 
